b6edfc84bf4df127e9210540e5ea82d601c41121
rbverissimo <rverissimo.dev@gmail.com>
2023-07-21 06:54:10 -0300

    Aprendendo a parametrizar as routas e usar aspas duplas para a construção de uma string interpolada.

--- routes/web.php
+++ routes/web.php
@@ -31,4 +31,8 @@ Route::get('/sobre-nos', 'SobreNosController@sobreNos');
 
 Route::get('/contato', 'ContatoController@contato');
 
+Route::get('/contato/{nome}/{categoria}/{assunto}/{mensagem}', function(string $nome, string $categoria, string $assunto, string $mensagem) {
+    echo "Estamos aqui, $nome   -  $categoria  -  $assunto  -  $mensagem"; 
+});
+
 // Route::get($uri, $callback);

4a4114fef64486f6581d844a1910e46455e79c08
rbverissimo <rverissimo.dev@gmail.com>
2023-07-24 18:58:55 -0300

    Aprendendo a passar parâmetros para a view usando o array associativo ao método view.

--- app/Http/Controllers/TesteController.php
+++ app/Http/Controllers/TesteController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class TesteController extends Controller
 {
     function teste(string $param1 = null, string $param2 = null){
-        return view('site.teste');
+        return view('site.teste', ['param1'=> $param1, 'param2'=>$param2]);
     }
 }
--- resources/views/site/teste.blade.php
+++ resources/views/site/teste.blade.php
@@ -1,3 +1,5 @@
 <h3>Apenas um teste</h3>
 <br>
+<p>param1 = {{ $param1 }}</p>
+<p>param2 = {{ $param2 }}</p>
 <a href="{{ route('site.index') }}">Clique para voltar</a>

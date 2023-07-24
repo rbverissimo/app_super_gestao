ab5567f1e61f1566dbf53535c04d53bd7604212d
rbverissimo <rverissimo.dev@gmail.com>
2023-07-24 19:02:18 -0300

    Fazendo o mesmo processo do commit anterior de passar os parâmetros para a view, porém usando o método compactnativo do PHP

--- app/Http/Controllers/TesteController.php
+++ app/Http/Controllers/TesteController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 class TesteController extends Controller
 {
     function teste(string $param1 = null, string $param2 = null){
-        return view('site.teste', ['param1'=> $param1, 'param2'=>$param2]);
+        // return view('site.teste', ['param1'=> $param1, 'param2'=>$param2]);
+        return view('site.teste', compact('param1', 'param2'));
     }
 }

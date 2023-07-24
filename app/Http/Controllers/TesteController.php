0342ec6166e3ac12b40a83d0466967a6048754ad
rbverissimo <rverissimo.dev@gmail.com>
2023-07-24 19:05:48 -0300

    Fazendo o mesmo processo de commits anteriores, porém usando o with() junto ao view

--- app/Http/Controllers/TesteController.php
+++ app/Http/Controllers/TesteController.php
@@ -8,6 +8,7 @@ class TesteController extends Controller
 {
     function teste(string $param1 = null, string $param2 = null){
         // return view('site.teste', ['param1'=> $param1, 'param2'=>$param2]);
-        return view('site.teste', compact('param1', 'param2'));
+        // return view('site.teste', compact('param1', 'param2'));
+        return view('site.teste')->with('param1', $param1)->with('param2', $param2);
     }
 }

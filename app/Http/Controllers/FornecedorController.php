a4154716da34d3a88c76d3c34157ad6fdc78e36c
rbverissimo <rverissimo.dev@gmail.com>
2023-08-01 19:30:09 -0300

    Entendo um pouco mais sobre o funcionamento do isset e criação de array de objetos

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -7,7 +7,17 @@ use Illuminate\Http\Request;
 class FornecedorController extends Controller
 {
     public function index(){
-        $fornecedores = [0 => ['nome' => 'Fornecedor 1', 'status' => 'N']];
+        $fornecedores = [
+            0 => [
+                'nome' => 'Fornecedor 1', 
+                'status' => 'N', 
+                'cnpj' => '00.000.000/000-00'
+            ],
+            1 => [
+                'nome' => 'Fornecedor 2',
+                'status' => 'S',
+            ]
+            ];
         return view('app.fornecedor.index', compact('fornecedores'));
     }
 }
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -6,14 +6,14 @@
 @endphp
 
 {{-- Operador @if --}}
-{{-- @if(count($fornecedores) > 0 && count($fornecedores) < 10)
+@if(count($fornecedores) > 0 && count($fornecedores) < 10)
       <h3>Existem alguns fornecedores cadastrados</h3>
 @elseif(count($fornecedores) > 10)
       <'h3>Existem vários fornecedores cadastrados</h3>
 @else
       <h3>Ainda não existem fornecedores cadastrados</h3>
 @endif
---}}
+
 {{-- Operador @unless --}}
 
 {{-- Fornecedor: {{ $fornecedores[0]['nome'] }}
@@ -32,4 +32,16 @@ Status: {{ $fornecedores[0]['status'] }}
       Fornecedor: {{ $fornecedores[0]['nome']}}
       <br>
       Status: {{ $fornecedores[0]['status']}}
+      <br>
+      CNPJ: {{ $fornecedores[0]['cnpj'] }}
+@endisset
+<br>
+@isset($forncedores)
+      Fornecedor: {{ $fornecedores[1]['nome']}}
+      <br>
+      Status: {{ $fornecedores[1]['status']}}
+      <br>
+      @isset($fornecedores[1]['cnpj'])
+            CNPJ: {{ $fornecedores[1]['cnpj'] }}
+      @endisset
 @endisset

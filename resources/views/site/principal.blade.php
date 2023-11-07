e4703e883e5bacdb8dfa9a2bcf06600c2e22dbab
renatoFromRyzen7 <rverissimo.dev@gmail.com>
2023-11-07 20:05:42 -0300

    Usando o include para gerar o topo da página

--- resources/views/site/principal.blade.php
+++ resources/views/site/principal.blade.php
@@ -6,20 +6,7 @@
 @section('titulo', $titulo)
 
 @section('conteudo')
-            <div class="topo">
-                  <div class="logo">
-                        <img src="{{asset('img/logo.png')}}">
-                  </div>
-
-                  <div class="menu">
-                        <ul>
-                              <li><a href="{{ route('site.index') }}">Home</a></li>
-                              <li><a href="{{ route('site.sobrenos') }}">Sobre Nós</a></li>
-                              <li><a href="{{ route('site.contato') }}">Contato</a></li>
-                        </ul>
-                  </div>
-            </div>
-
+            @include('site.layouts._partials.topo')
             <div class="conteudo-destaque">    
                   <div class="esquerda">
                         <div class="informacoes">
--- resources/views/site/sobre-nos.blade.php
+++ resources/views/site/sobre-nos.blade.php
@@ -6,21 +6,7 @@
 @section('titulo', $titulo)
 
 @section('conteudo')
-        <div class="topo">
-
-            <div class="logo">
-                <img src="{{asset('img/logo.png')}}">
-            </div>
-
-            <div class="menu">
-                <ul>
-                    <li><a href="{{ route('site.index') }}">Home</a></li>
-                    <li><a href="{{ route('site.sobrenos') }}">Sobre Nós</a></li>
-                    <li><a href="{{ route('site.contato') }}">Contato</a></li>
-                </ul>
-            </div>
-        </div>
-
+        @include('site.layouts._partials.topo')
         <div class="conteudo-pagina">
             <div class="titulo-pagina">
                 <h1>Olá, eu sou o Super Gestão</h1>

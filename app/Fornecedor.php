b1e147b987006b21f5c557f249882822f3e3a645
rbverissimo <rverissimo.dev@gmail.com>
2023-12-13 06:49:15 -0300

    Compreendendo o uso do $table e do $fillable

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -4,7 +4,10 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+// fornecedors --> nomeação automática do laravel
 class Fornecedor extends Model
 {
-    //
+    protected $table = 'fornecedores'; // indicação do nome da tabela
+
+    protected $fillable = ['nome', 'site', 'email', 'uf'];
 }

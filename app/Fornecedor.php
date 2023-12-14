3fbd329ac704ad3b0a4008f703ec9d4600065d09
rbverissimo <rverissimo.dev@gmail.com>
2023-12-14 07:16:16 -0300

    Entendendo sobre o where e adicionando o fillable ao SiteContato

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -13,4 +13,6 @@ class Fornecedor extends Model
 
     //.all(); recupera toda a Collection do banco
     //.find($id...); recupera objeto de acordo com o(s) id(s) passado;
+
+    //where('coluna', 'operador', valor)->get(); 
 }
--- app/SiteContato.php
+++ app/SiteContato.php
@@ -7,4 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class SiteContato extends Model
 {
     //.save() --> salva o objeto no banco de dados;
+
+    protected $fillable = ['nome', 'telefone', 'email', 'motivo_contato', 'mensagem'];
 }

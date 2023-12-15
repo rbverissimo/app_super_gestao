86a18df2b33752d93fdd0b8e99f98ee23778c88f
rbverissimo <rverissimo.dev@gmail.com>
2023-12-15 06:59:20 -0300

    Entendendo sobre o whereIn e o whereNotIn

--- app/SiteContato.php
+++ app/SiteContato.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class SiteContato extends Model
 {
     //.save() --> salva o objeto no banco de dados;
+    // use \App\SiteContato;
+    // ::whereIn('campo_a_ser_comparado_por_igual', 'conjunto_de_param');
+    // ::whereNotIn() é a mesma ideia do whereIn, mas as comparações excluem os parâmetros passados
 
     protected $fillable = ['nome', 'telefone', 'email', 'motivo_contato', 'mensagem'];
 }

332a89c9a9cefdfdbd102123bdd8278a83387a15
renatoFromRyzen7 <rverissimo.dev@gmail.com>
2024-04-10 20:29:55 -0300

    Associando o autenticação middleware à rota

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,9 @@ Route::get('/login', function(){ return 'Login'; })->name('site.login');
 
 // /app
 Route::prefix('/app')->group(function(){
-    Route::get('/clientes', function(){ return 'Clientes'; })->name('app.clientes');
+    Route::middleware('autenticacao', 'log.acesso')
+        ->get('/clientes', function(){ return 'Clientes'; })
+        ->name('app.clientes');
     Route::get('/fornecedores','FornecedorController@index')->name('app.fornecedores');
     Route::get('/produtos', function(){ return 'Produtos'; })->name('app.produtos');
 });

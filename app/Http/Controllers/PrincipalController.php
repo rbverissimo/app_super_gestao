aef2a30ef3935c49402d35bd4c260edad2aec3e8
renatoFromRyzen7 <rverissimo.dev@gmail.com>
2024-04-17 09:51:22 -0300

    Ajustando a rota no web.php para comportar o formulário de contato

--- app/Http/Controllers/PrincipalController.php
+++ app/Http/Controllers/PrincipalController.php
@@ -7,6 +7,14 @@ use Illuminate\Http\Request;
 class PrincipalController extends Controller
 {
     public function principal() {
-        return view('site.principal', ['titulo' => 'Home (teste)']);
+
+        $motivos_contatos = [
+            '1' => 'Dúvida',
+            '2' => 'Elogio',
+            '3' => 'Reclamação'
+        ];
+
+
+        return view('site.principal', ['titulo' => 'Home (teste)', 'motivo_contato' => $motivos_contatos]);
     }
 }
